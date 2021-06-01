24c6910f0fd397d1b3715ef8fbc6bb7980cea886
ali <alihkhan98@gmail.com>
2021-06-01 19:56:00 +1000

    push

--- labs/lab_01/no_vowels.c
+++ labs/lab_01/no_vowels.c
@@ -2,5 +2,16 @@
 #include <string.h>
 
 int main(void) {
+	
+	char string[100];
+
+	//scanf("%[^\n]", string)
+	
+	while ( scanf("%s", string) != EOF )
+	{
+		printf("%s\n", string);
+	}
+	
 	return 0;
+	
 }
