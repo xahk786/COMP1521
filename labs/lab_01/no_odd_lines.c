e3d2879c4098d008e3cc208b66a37eff035c3307
ali <alihkhan98@gmail.com>
2021-06-01 21:16:30 +1000

    done

--- labs/lab_01/no_odd_lines.c
+++ labs/lab_01/no_odd_lines.c
@@ -2,5 +2,22 @@
 #include <string.h>
 
 int main(void) {
+
+	char str[1024];
+
+	while (fgets(str, 1024, stdin) != NULL)
+	{
+		int size = strlen(str);
+
+		if (size % 2 == 0)
+		{
+			fputs(str, stdout);	
+		}
+		
+	}
+
+	
+
+	
 	return 0;
 }
