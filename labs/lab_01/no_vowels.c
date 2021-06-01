65e29faa20792eb56c4431a6086c3630db467f95
ali <alihkhan98@gmail.com>
2021-06-01 20:07:50 +1000

    push

--- labs/lab_01/no_vowels.c
+++ labs/lab_01/no_vowels.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+int not_vowel(char letter);
+
 int main(void) {
 	
-	char string[100];
+	char ch;
 
-	//scanf("%[^\n]", string)
-	
-	while ( scanf("%s", string) != EOF )
+	while ( scanf("%c", &ch) != EOF)
 	{
-		printf("%s\n", string);
+		if (not_vowel(ch))
+		{
+			printf("%c", ch);	
+		}
+		
 	}
 	
 	return 0;
 	
+}
+
+int not_vowel(char letter){
+	
+	int is_true;
+	if (letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U' && letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u')
+	{
+		is_true = 1;
+	} else 
+	{
+		is_true = 0;
+	}
+
+	return is_true;
 }
