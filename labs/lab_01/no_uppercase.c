1688a1e409df952a55184249b0042f66ee7bb23a
ali <alihkhan98@gmail.com>
2021-06-01 20:37:37 +1000

    uppercaseone

--- labs/lab_01/no_uppercase.c
+++ labs/lab_01/no_uppercase.c
@@ -2,5 +2,41 @@
 #include <ctype.h>
 
 int main(void) {
+
+	char ch;
+
+	while ( ch = getchar() != EOF )
+	{
+
+		if (ch > 'A' && ch < 'Z')
+		{
+			char answ = ch + 32;
+
+			putchar(answ);
+		} else 
+		{
+			putchar(ch);
+		}
+
+	
+	}
+
+	char answ = 'a' - 32;
+
+	printf("%c,", answ);
+		
 	return 0;
+}
+
+int convert(char letter)
+{
+	if (letter > 'A' && letter < 'Z') 
+	{
+		char answ = letter + 32;
+
+		return answ
+	}
+
+	
+
 }
