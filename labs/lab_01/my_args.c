60eda14b7884037e76d3b6807a97c18c4ac7a929
ali <alihkhan98@gmail.com>
2021-06-01 21:31:00 +1000

    add

--- labs/lab_01/my_args.c
+++ labs/lab_01/my_args.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	(void) argc, (void) argv; // keep the compiler quiet, should be removed
+	//(void) argc, (void) argv; // keep the compiler quiet, should be removed
+
+	printf("Program name: %s\n", argv[0]);
+
+	if (argc == 0)
+	{
+		printf("There are no other arguments");
+	} else 
+	{
+		printf("There are %d arguments:\n", argc-1);
+			for (int i = 1; i < argc; i++)
+			{
+				printf("\tArgument %d is \"%s\"\n", i, argv[i]);
+			}
+
+	}
+
+
+
 	return 0;
 }
