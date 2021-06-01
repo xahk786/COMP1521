aed23a0785d41e30fc8ca0f80d88d0aef85a50c4
ali <alihkhan98@gmail.com>
2021-06-01 22:13:13 +1000

    push

--- labs/lab_01/arg_stats.c
+++ labs/lab_01/arg_stats.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int find_min(void);
+
 int main(int argc, char **argv) {
-	(void) argc, (void) argv; // keep the compiler quiet, should be removed
+	//(void) argc, (void) argv; // keep the compiler quiet, should be removed
+
+	int min;
+	int max;
+	int sum = 0;
+	int prod = 1;
+	int mean;
+
+	
+	for (int i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) < min)
+		{
+			min = atoi(argv[i]);
+		}
+		
+		if (atoi(argv[i]) > max)
+		{
+			max = atoi(argv[i]);
+		}
+
+		sum = sum + atoi(argv[i]);
+		prod = prod * atoi(argv[i]);
+		//printf("%d %d\n", atoi(argv[i]), min);	
+	}
+	
+	mean = sum / (argc - 1);
+	printf("MIN:  %d\n", min);
+	printf("MAX:  %d\n", max);
+	printf("SUM:  %d\n", sum);
+	printf("PROD:  %d\n", prod);
+	printf("MEAN:  %d\n", mean);
+
+	
 	return 0;
 }
