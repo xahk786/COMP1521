c0b494fe6ef6e4ecdaa967eb7ac505cf90c270a0
ali <alihkhan98@gmail.com>
2021-06-03 17:31:08 +1000

    passing on cse now

--- labs/lab_01/arg_stats.c
+++ labs/lab_01/arg_stats.c
@@ -6,11 +6,11 @@ int find_min(void);
 int main(int argc, char **argv) {
 	//(void) argc, (void) argv; // keep the compiler quiet, should be removed
 
-	int min;
-	int max;
+	int min = atoi(argv[1]);
+	int max = atoi(argv[1]);
 	int sum = 0;
 	int prod = 1;
-	int mean;
+	int mean = 0;
 
 	
 	for (int i = 1; i < argc; i++)
@@ -34,8 +34,8 @@ int main(int argc, char **argv) {
 	printf("MIN:  %d\n", min);
 	printf("MAX:  %d\n", max);
 	printf("SUM:  %d\n", sum);
-	printf("PROD:  %d\n", prod);
-	printf("MEAN:  %d\n", mean);
+	printf("PROD: %d\n", prod);
+	printf("MEAN: %d\n", mean);
 
 	
 	return 0;
--- labs/lab_01/my_args.c
+++ labs/lab_01/my_args.c
@@ -5,9 +5,9 @@ int main(int argc, char **argv) {
 
 	printf("Program name: %s\n", argv[0]);
 
-	if (argc == 0)
+	if (argc <= 1)
 	{
-		printf("There are no other arguments");
+		printf("There are no other arguments\n");
 	} else 
 	{
 		printf("There are %d arguments:\n", argc-1);
